1419ad24abff8df559133848464d5850861e2b72
Keaton Greve <keatongreve@gmail.com>
2015-06-11 13:29:36 -0500

    Fix RC infotizer for PRs with > 250 commits
    Gotta run real git commands on a repo.

--- gatekeeper/rc_infotizer.rb
+++ gatekeeper/rc_infotizer.rb
@@ -17,23 +17,23 @@ parser = OptionParser.new do |opts|
   opts.on('-l', '--login login', 'GitHub login (username or email)') do |login|
     options[:login] = login
   end
-
+  
   opts.on('-p', '--password password', 'GitHub password') do |password|
     options[:password] = password
   end
-
+  
   opts.on('-t', '--token token', 'GitHub access token (overrides username and password)') do |token|
     options[:token] = token
   end
-
+  
   opts.on('--repo repo', 'Repository name') do |repo|
     options[:repo] = repo
   end
-
+  
   opts.on('-m', '--master master', 'Master/stable branch (default is master)') do |master_branch|
     options[:master_branch] = master_branch
   end
-
+  
   opts.on('--release release', 'Release candidate branch name (pull request should already be open)') do |release_branch|
     options[:release_branch] = release_branch
   end
@@ -65,6 +65,17 @@ user = client.user
 
 begin
   repo = client.repository(options[:repo])
+  puts "Cloning/pulling git repo #{options[:repo]} on another thread"
+  git_repo_dir = "infotizer_tmp_#{options[:repo]}".gsub!(/\W/,'')
+  thread = Thread.new {
+    if Dir.exist? git_repo_dir
+      puts "dir exists, pulling"
+      `cd #{git_repo_dir} && git pull`
+    else
+      "dir does not exist, cloning new"
+      `git clone git@github.com:#{options[:repo]}.git #{git_repo_dir}`
+    end
+  }
 rescue Octokit::NotFound
   abort "Repo '#{options[:repo]} could not be found."
 end
@@ -78,24 +89,34 @@ end
 open_pulls = client.pull_requests(options[:repo], { state: 'open', base: options[:master_branch] })
 
 rc_pull = open_pulls.select { |p| p[:head][:ref] == options[:release_branch] }.first
-rc_commits = client.pull_request_commits(options[:repo], rc_pull[:number])
 
-rc_commits_formatted = rc_commits.map do |commit|
-    commit_msg = commit[:commit][:message]
-    newline_index = commit_msg.index("\n")
-    commit_msg = commit_msg[0..newline_index].strip unless newline_index.nil?
-    { url: commit[:html_url], message: commit_msg }
+thread.join
+
+Dir.chdir git_repo_dir
+master_commits = `git log --oneline --reverse origin/#{options[:master_branch]}`.split("\n")
+rc_commits = `git log --oneline --reverse origin/#{options[:release_branch]}`.split("\n")
+
+rc_commits_diff = rc_commits - master_commits
+puts rc_commits_diff
+puts rc_commits_diff.length
+
+rc_commit_links = rc_commits_diff.map do |commit|
+  c = {}
+  match = commit.match /Merge pull request #(\d*)/
+  if match
+    c[:pr_number] = match.captures[0] 
+    c[:text] = commit.split(" ")[1..commit.split(" ").length-1].join(" ")
+    c[:url] = "https://github.com/#{options[:repo]}/pull/#{c[:pr_number]}"
+    c
+  else
+    nil
   end
+end.compact
 
-# PRs into develop before the RC was created
-rc_features = rc_commits_formatted.select { |c|
-  match = c[:message].match /Merge pull request #(\d*)/
-  c[:pr_number] = match.captures[0] if match
-  match
-}
+body_text =  rc_commit_links.map do |c| 
+ "[#{c[:text]}](#{c[:url]})"
+end.join("\n")
 
-body_text = rc_features.map { |c| "[#{c[:message]}](https://github.com/#{options[:repo]}/pull/#{c[:pr_number]})" }.join("\n")
-
-puts rc_pull[:number]
+puts "RC pull request \# is #{rc_pull[:number]}"
 puts body_text
 client.update_pull_request(options[:repo], rc_pull[:number], { body: body_text })
